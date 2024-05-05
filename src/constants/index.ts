8ad7ea2a30d7f2de8eb5535a3d0757b124bb72aa
Ankit Yadav <ankityadav.webdev@gmail.com>
2024-05-05 11:32:08 +0530

    setup project folder structure.

---
+++ src/constants/index.ts
@@ -0,0 +1,7 @@
+export const sideBarLinks = [
+    { label: 'Home', route: '/', imgUrl: '/icons/something.svg' },
+    { label: 'Upcoming', route: '/upcoming', imgUrl: '/icons/upcoming.svg' },
+    { label: 'Previous', route: '/previous', imgUrl: '/icons/previous.svg' },
+    { label: 'Recordings', route: '/recordings', imgUrl: '/icons/Video.svg' },
+    { label: 'Personal', route: '/personal-room', imgUrl: '/icons/add-personal.svg' },
+];
